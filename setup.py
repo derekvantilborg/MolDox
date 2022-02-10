aee5e69b492be5cb3c0361e3be25bc31446451f0
derekvantilborg <dwvantilborg@gmail.com>
2022-02-10 15:06:02 +0100

    Fixed some conda build stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MolDox',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/derekvantilborg/MolDox',
+    license='MIT',
+    author='Derek van Tilborg',
+    author_email='d.w.v.tilborg@tue.nl',
+    description='Easy Molecular Docking'
+)
